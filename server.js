8380d568d431b8da4444526a9bc9f58cec083f8f
SebastianV713 <vaughanbusiness13@gmail.com>
2026-02-13 20:04:03 -0800

    chore: add request logging to debug missing webhooks

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ emailScheduler.startEmailScheduler();
 
 const app = express();
 
+// Log all incoming requests for debugging
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
+
 // Configure CORS
 app.use(cors({
     origin: function (origin, callback) {
